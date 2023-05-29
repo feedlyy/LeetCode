01fb1ffc0c25ce06003822ea89f14f1eeeed9471
Muhammad Nur Fadli <muhammad.fadli@lemonilo.com>
2023-05-29 16:36:30 +0700

    nerge string alternately

---
+++ 1768.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = ""
+        iteration = ""
+
+        if len(word1) > len(word2):
+            iteration = word2
+        else:
+            iteration = word1
+
+        for i in range(len(iteration)):
+            result += word1[i] + word2[i]
+
+        if len(word1) == len(word2):
+            return result
+
+        if iteration == word1:
+            result += word2[len(iteration):]
+        else:
+            result += word1[len(iteration):]
+
+        return result
